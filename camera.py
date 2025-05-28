48dace41c593c9d6eba5484a5f00e07e9426ef8a
Johanness Ramiandrisoa <johannessram@gmail.com>
2025-05-28 08:08:02 +0300

    no details when KeyError

--- camera.py
+++ camera.py
@@ -78,6 +78,6 @@ if uploaded_file is not None:
         except:
             if not isinstance(predict_image, list):
                 prediction = [prediction]
-    st.write(f"**Prediction:** {' '.join(prediction)}")
+    st.write(f"### **Prediction:** {' '.join(prediction)}")
     st.write(details_)
     os.remove(image_path)
